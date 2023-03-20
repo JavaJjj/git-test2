da94c90c6bbaa252d16675a38f048c7b9feb6f4d
zhangsan <zhangsan@.com>
2023-03-20 12:17:56 +0800

    fix: second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("审批来了吗");
+        System.out.println("审批来了吗，第二次修改");
     }
 }
