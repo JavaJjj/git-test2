dba346d2037fe8aee32f3253524e239508161b2d
zhangsan <zhangsan@.com>
2023-03-20 14:03:15 +0800

    fix: push test commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("审批来了吗，第四次修改");
         System.out.println("审批来了吗，master test");
         System.out.println("审批来了吗，develop test");
+        System.out.println("审批来了吗，push test");
     }
 }
