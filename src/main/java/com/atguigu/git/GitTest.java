55094ac72e3696941f6a8366ee202277962dd297
JavaJjj <120066991+JavaJjj@users.noreply.github.com>
2023-03-20 14:08:03 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("审批来了吗，develop test");
         System.out.println("审批来了吗，push test");
         System.out.println("审批来了吗，push ssh test");
+        System.out.println("审批来了吗，pull test");
     }
 }
